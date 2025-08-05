23ac47929ec527986c66bc21ec3ad09160203c33
tiau1988 <tiau88@gmail.com>
2025-08-05 17:06:24 +0800

    Fix db-check endpoint export syntax

--- api/db-check.js
+++ api/db-check.js
@@ -1,4 +1,4 @@
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
