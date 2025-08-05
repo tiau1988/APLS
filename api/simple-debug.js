d48ae6cdbce36f992c3aa7f73cdea44d66714b2b
tiau1988 <tiau88@gmail.com>
2025-08-05 16:51:11 +0800

    Fix simple-debug endpoint syntax to use CommonJS format

--- api/simple-debug.js
+++ api/simple-debug.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -56,4 +56,4 @@ export default function handler(req, res) {
       stack: error.stack
     });
   }
-}
+};
